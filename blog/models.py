bf888e1a42d6671303c7e20f5b6ecba45ed414fc
Larry Lira <larrylira@larrys-mbp.attlocal.net>
2019-09-09 17:31:21 -0500

    show all blog posts on home page

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-def Comment(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
